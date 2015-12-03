c1c5be38596791466213cf16bab2a8e0725abefa
Piotr Lisiecki <piotr.lisiecki@cognifide.com>
2015-12-03 09:33:11 +0100

    Added multiple search after space with highlight

--- dist/typeahead.bundle.js
+++ dist/typeahead.bundle.js
@@ -1300,25 +1300,50 @@
             caseSensitive: false
         };
         return function hightlight(o) {
-            var regex;
+            var regexTable;
             o = _.mixin({}, defaults, o);
             if (!o.node || !o.pattern) {
                 return;
             }
             o.pattern = _.isArray(o.pattern) ? o.pattern : [ o.pattern ];
-            regex = getRegex(o.pattern, o.caseSensitive, o.wordsOnly);
+            regexTable = getRegex(o.pattern, o.caseSensitive, o.wordsOnly);
             traverse(o.node, hightlightTextNode);
             function hightlightTextNode(textNode) {
-                var match, patternNode, wrapperNode;
-                if (match = regex.exec(textNode.data)) {
-                    wrapperNode = doc.createElement(o.tagName);
-                    o.className && (wrapperNode.className = o.className);
-                    patternNode = textNode.splitText(match.index);
-                    patternNode.splitText(match[0].length);
-                    wrapperNode.appendChild(patternNode.cloneNode(true));
-                    textNode.parentNode.replaceChild(wrapperNode, patternNode);
+                var matchNode, wrapperNode, match, allMatch = true, text = textNode.data, matchesTable = [];
+                _.each(regexTable, function(regex) {
+                    if (regex.exec(text)) {
+                        match = regex.exec(text);
+                        matchesTable.push({
+                            index: match.index,
+                            length: match[0].length
+                        });
+                        text = text.substr(0, match.index) + getPipesString(match[0].length) + text.substr(match.index + match[0].length);
+                    } else {
+                        allMatch = false;
+                        return false;
+                    }
+                });
+                if (allMatch) {
+                    matchesTable.sort(sortFromLast);
+                    _.each(matchesTable, function(match) {
+                        matchNode = textNode.splitText(match.index);
+                        matchNode.splitText(match.length);
+                        wrapperNode = doc.createElement(o.tagName);
+                        wrapperNode.appendChild(matchNode.cloneNode(true));
+                        matchNode.parentNode.replaceChild(wrapperNode, matchNode);
+                    });
                 }
-                return !!match;
+                return !!allMatch;
+            }
+            function getPipesString(length) {
+                var str = "", i;
+                for (i = 0; i < length; i++) {
+                    str = str + "|";
+                }
+                return str;
+            }
+            function sortFromLast(a, b) {
+                return a.index > b.index ? -1 : 1;
             }
             function traverse(el, hightlightTextNode) {
                 var childNode, TEXT_NODE_TYPE = 3;
@@ -1333,12 +1358,17 @@
             }
         };
         function getRegex(patterns, caseSensitive, wordsOnly) {
-            var escapedPatterns = [], regexStr;
+            var escapedPatterns = [], regexStr, regexTable;
             for (var i = 0, len = patterns.length; i < len; i++) {
                 escapedPatterns.push(_.escapeRegExChars(patterns[i]));
             }
-            regexStr = wordsOnly ? "\\b(" + escapedPatterns.join("|") + ")\\b" : "(" + escapedPatterns.join("|") + ")";
-            return caseSensitive ? new RegExp(regexStr) : new RegExp(regexStr, "i");
+            regexStr = escapedPatterns[0];
+            regexTable = regexStr.trim().split(/\s+/g);
+            _.each(regexTable, function(str, i) {
+                str = wordsOnly ? "\\b(" + str + ")\\b" : "(" + str + ")";
+                regexTable[i] = caseSensitive ? new RegExp(str) : new RegExp(str, "i");
+            });
+            return regexTable;
         }
     }(window.document);
     var Input = function() {
