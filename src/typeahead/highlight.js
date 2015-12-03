d67fe7779b444fec13bfd51ee9090c6fdf44d724
Piotr Lisiecki <piotr.lisiecki@cognifide.com>
2015-12-03 09:29:16 +0100

    Added multiple search after space with highlight

--- src/typeahead/highlight.js
+++ src/typeahead/highlight.js
@@ -6,79 +6,96 @@
 
 // inspired by https://github.com/jharding/bearhug
 
-var highlight = (function(doc) {
-  'use strict';
-
-  var defaults = {
+var highlight = function(doc) {
+    "use strict";
+    var defaults = {
         node: null,
         pattern: null,
-        tagName: 'strong',
+        tagName: "strong",
         className: null,
         wordsOnly: false,
         caseSensitive: false
-      };
+    };
+    return function hightlight(o) {
+        var regexTable;
+        o = _.mixin({}, defaults, o);
+        if (!o.node || !o.pattern) {
+            return;
+        }
+        o.pattern = _.isArray(o.pattern) ? o.pattern : [ o.pattern ];
+        regexTable = getRegex(o.pattern, o.caseSensitive, o.wordsOnly);
+        traverse(o.node, hightlightTextNode);
+        function hightlightTextNode(textNode) {
+            var matchNode, wrapperNode,
+              match,
+              allMatch = true,
+              text = textNode.data,
+              matchesTable = [];
 
-  return function hightlight(o) {
-    var regex;
+          _.each(regexTable, function(regex) {
+            if (regex.exec(text)) {
+	            match = regex.exec(text);
+	            matchesTable.push({index: match.index, length: match[0].length});
+	            text = text.substr(0, match.index) + getPipesString(match[0].length) +
+			            text.substr(match.index + match[0].length);
+            } else {
+	            allMatch = false;
+	            return false;
+            }
+          });
 
-    o = _.mixin({}, defaults, o);
+          if (allMatch) {
+            matchesTable.sort(sortFromLast);
+            _.each(matchesTable, function(match) {
+	            matchNode = textNode.splitText(match.index);
+	            matchNode.splitText(match.length);
 
-    if (!o.node || !o.pattern) {
-      // fail silently
-      return;
-    }
+	            wrapperNode = doc.createElement(o.tagName);
+	            wrapperNode.appendChild(matchNode.cloneNode(true));
 
-    // support wrapping multiple patterns
-    o.pattern = _.isArray(o.pattern) ? o.pattern : [o.pattern];
-
-    regex = getRegex(o.pattern, o.caseSensitive, o.wordsOnly);
-    traverse(o.node, hightlightTextNode);
-
-    function hightlightTextNode(textNode) {
-      var match, patternNode, wrapperNode;
-
-      if (match = regex.exec(textNode.data)) {
-        wrapperNode = doc.createElement(o.tagName);
-        o.className && (wrapperNode.className = o.className);
-
-        patternNode = textNode.splitText(match.index);
-        patternNode.splitText(match[0].length);
-        wrapperNode.appendChild(patternNode.cloneNode(true));
-
-        textNode.parentNode.replaceChild(wrapperNode, patternNode);
-      }
-
-      return !!match;
-    }
-
-    function traverse(el, hightlightTextNode) {
-      var childNode, TEXT_NODE_TYPE = 3;
-
-      for (var i = 0; i < el.childNodes.length; i++) {
-        childNode = el.childNodes[i];
-
-        if (childNode.nodeType === TEXT_NODE_TYPE) {
-          i += hightlightTextNode(childNode) ? 1 : 0;
+	            matchNode.parentNode.replaceChild(wrapperNode, matchNode);
+            });
+          }
+          return !!allMatch;
         }
 
-        else {
-          traverse(childNode, hightlightTextNode);
+        function getPipesString(length) {
+          var str = '', i;
+          for (i = 0; i < length; i++) {
+  	        str = str + '|';
+          }
+          return str;
         }
-      }
+
+        function sortFromLast(a, b) {
+          return a.index > b.index ? -1 : 1
+        }
+
+        function traverse(el, hightlightTextNode) {
+            var childNode, TEXT_NODE_TYPE = 3;
+            for (var i = 0; i < el.childNodes.length; i++) {
+                childNode = el.childNodes[i];
+                if (childNode.nodeType === TEXT_NODE_TYPE) {
+                    i += hightlightTextNode(childNode) ? 1 : 0;
+                } else {
+                    traverse(childNode, hightlightTextNode);
+                }
+            }
+        }
+    };
+    
+    function getRegex(patterns, caseSensitive, wordsOnly) {
+        var escapedPatterns = [], regexStr, regexTable;
+        for (var i = 0, len = patterns.length; i < len; i++) {
+            escapedPatterns.push(_.escapeRegExChars(patterns[i]));
+        }
+      regexStr = escapedPatterns[0];
+      regexTable = regexStr.trim().split(/\s+/g);
+      _.each(regexTable, function(str, i){
+        str = wordsOnly ? "\\b(" + str + ")\\b" : "(" + str + ")";
+        regexTable[i] =  caseSensitive ? new RegExp(str) : new RegExp(str, "i");
+      });
+
+        return regexTable;
     }
-  };
-
-  function getRegex(patterns, caseSensitive, wordsOnly) {
-    var escapedPatterns = [], regexStr;
-
-    for (var i = 0, len = patterns.length; i < len; i++) {
-      escapedPatterns.push(_.escapeRegExChars(patterns[i]));
-    }
-
-    regexStr = wordsOnly ?
-      '\\b(' + escapedPatterns.join('|') + ')\\b' :
-      '(' + escapedPatterns.join('|') + ')';
-
-    return caseSensitive ? new RegExp(regexStr) : new RegExp(regexStr, 'i');
-  }
-})(window.document);
+}(window.document);
